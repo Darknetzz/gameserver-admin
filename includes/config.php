28203edb61b4b8460bd5e224d959272166f8e556
Darknetzz <github@roste.org>
2022-04-29 00:27:50 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,11 +3,12 @@
 
 # General
 $cfg['title'] = "PHPGSAdmin";       # Friendly name shown in browser
+$cfg['logo'] = null;                # Link or filepath to logo in navbar
 
 # Security
-$cfg['adminUsername'] = "admin";    # Initial administrator username
+$cfg['adminUsername'] = "admin";     # Initial administrator username
 $cfg['$adminPassword'] = "CHANGEME"; # Initial administrator password
 $cfg['$adminEnabled']  = true;       # Enable admin account, you should falsify this after creating a custom account
 $cfg['$pepper'] = "CHANGEME";        # Change this to a random string (up to 255 chars)
-$cfg['$requireLogin'] = true;        # Should always be true
+$cfg['$requireLogin'] = true;        # Should always be true unless secured otherwise
 ?>
