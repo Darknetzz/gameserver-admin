23b75f3c00dbf47332ab142dea97eab4ccda6e02
Darknetzz <github@roste.org>
2022-04-30 00:19:47 +0200

    Create sqlcon.example.php

---
+++ includes/sqlcon.example.php
@@ -0,0 +1,9 @@
+<?php
+# Change this to your SQL parameters and save as sqlcon.php
+$host = "localhost";
+$db   = "serveradmin";
+$user = "root";
+$pass = "";
+
+$sqlcon = mysqli_connect($host,$user,$pass,$db);
+?>
