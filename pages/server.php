7576ac481a8224e9a0cdd69237eef51e53befb56
Darknetzz <github@roste.org>
2022-04-30 02:49:44 +0200

    color change

--- pages/server.php
+++ pages/server.php
@@ -73,7 +73,7 @@ while ($server = $result->fetch_assoc()) {
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-success">Save changes</button>
             </div>
             </div>
         </div>
