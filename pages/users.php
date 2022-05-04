3d2457e09b1da2976ee2b349c0abb04bb958a06f
Darknetzz <github@roste.org>
2022-05-04 14:58:58 +0200

    cleanup users table

--- pages/users.php
+++ pages/users.php
@@ -7,17 +7,19 @@ echo "<table class='table table-".CFG_TABLESTYLE."'>
 <tr class='bg-".CFG_TABLEHEADERCOLOR."'>
     <th>Username</th>
     <th>Role</th>
+    <th>SSH</th>
+    <th>Web</th>
 </tr>";
 while ($user = $getUsers->fetch_assoc()) {
-    if ($user['ssh'] == 1) {
-        $sshEnabled = "(SSH-enabled)";
-    } else {
-        $sshEnabled = null;
-    }
+    $sshEnabled = ($user['ssh'] == 1) ? "<span class='badge bg-success'>Enabled</span>" : "<span class='badge bg-danger'>Disabled</span>";
+    $webEnabled = ($user['web'] == 1) ? "<span class='badge bg-success'>Enabled</span>" : "<span class='badge bg-danger'>Disabled</span>";
+
     echo "
     <tr>
     <td><a href='?p=user&id=$user[id]'>$user[username]</a></td>
-    <td>".translateRole($user['role'])." $sshEnabled</td>
+    <td>".translateRole($user['role'])."</td>
+    <td>$sshEnabled</td>
+    <td>$webEnabled</td>
     </tr>";
 }
 echo "</table>";
